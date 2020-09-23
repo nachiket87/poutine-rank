0d82930e34385d6439107f2e8dc594ea63eb0dd9
Nachiket Pusalkar <nachiket87@gmail.com>
2020-09-23 18:26:39 -0400

    :fire: typescript appears to be working :fire:

--- app/javascript/components/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return (<div>WHAT NACHIKET!</div>)
-}
-
-export default App
-
-
---
+++ app/javascript/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return(
+  <div>
+    <h1>AWESOME BLOSSOM</h1>
+    WHAT NACHIKET!
+    <h2> NACHIKET PUSALKAR </h2>
+  </div>)
+}
+
+export default App
+
+
