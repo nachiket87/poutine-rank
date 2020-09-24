be7fbe145d1951d9d079ac4a81acb797215eff83
Nachiket Pusalkar <nachiket87@gmail.com>
2020-09-23 23:18:43 -0400

    :fire: React works

--- app/javascript/components/Poutines/Poutines.tsx
+++ app/javascript/components/Poutines/Poutines.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 
 const Poutines: React.FC = () => {
-  return <div> This is the Poutines#Index view </div>
-}
+  const [poutines, setPoutines] = useState([])
+
+  useEffect( () => {
+    // get all poutines from API
+    // Update poutine in our state
+   fetch('/api/v1/poutines.json')
+      .then( resp => resp.json())
+      .then( data => {
+        setPoutines(data.data)
+      })
+      .catch( resp => console.log(resp))
+  }, [poutines.length])
+
+  const list = poutines.map( item => {
+    return(<li key={item.attributes.name}>{ item.attributes.name }</li>)
+  })
+
+  return (
+    <Fragment>
+      <div> This is the Poutines#Index view </div>
+      <ul>{list}</ul>
+    </Fragment>
+  )}
 
 export default Poutines
