6fe288946083822e099641b44fb31cb8b8c39b86
Nachiket Pusalkar <nachiket87@gmail.com>
2020-11-24 15:32:25 -0500

    updates for CORS

--- app/javascript/components/Poutine/Header.tsx
+++ app/javascript/components/Poutine/Header.tsx
@@ -1,20 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Header = (props) => {
-    const {name, image_url, avg_score} = props.attributes.data.attributes
-    const total = props.attributes.data.relationships.reviews.data.length
-    console.log(props.attributes.data.relationships.reviews.data.length)
-    return (
-      <div className='wrapper'>
-        <div> 
-          <p>name: {name}</p>
-          <p>image_url: {image_url}</p>
-          <p>avg_score: {avg_score}</p>
-          <p>total reviews: {total}</p>
-        </div>
-
+  const { name, image_url, avg_score } = props.attributes.data.attributes;
+  const total = props.attributes.data.relationships.reviews.data.length;
+  return (
+    <div className="wrapper">
+      <div>
+        <p>name: {name}</p>
+        <img src={image_url} style={{ width: "100px" }} />
+        <p>image_url: {image_url}</p>
+        <p>avg_score: {avg_score}</p>
+        <p>total reviews: {total}</p>
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- app/javascript/components/Poutines/Poutine.tsx
+++ app/javascript/components/Poutines/Poutine.tsx
@@ -1,32 +1,32 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import styled from 'styled-components'
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 interface attrib {
-  name: string,
-  image_url: string,
-  avg_score: number,
-  slug: string
+  name: string;
+  image_url: string;
+  avg_score: number;
+  slug: string;
 }
 
 const Card = styled.div`
- justify-content: center;
- border: 1px solid #efefef;
- background: white;
-`
-const Poutineimage = styled.div`
- width: 50px;
- margin: 10px auto;
-
- img {
-  height: 50px;
-  width: 50px;
+  justify-content: center;
   border: 1px solid #efefef;
- }
-`
+  background: white;
+`;
+const Poutineimage = styled.div`
+  width: 50px;
+  margin: 10px auto;
+
+  img {
+    height: 50px;
+    width: 50px;
+    border: 1px solid #efefef;
+  }
+`;
 const Poutinename = styled.div`
   padding: 20px 0 10px 0;
-`
+`;
 const Linkwrapper = styled.div`
   margin: 30px 0 20px 0;
   height: 50px;
@@ -39,28 +39,24 @@ const Linkwrapper = styled.div`
     padding: 1rem;
     border: 1px solid black;
   }
-`
+`;
 
-
-
-const Poutine = (props: {attributes: attrib}) => {
+const Poutine = (props: { attributes: attrib }) => {
   return (
     <Card>
       <Poutineimage>
         <img src={props.attributes.image_url} alt={props.attributes.name} />
       </Poutineimage>
-      <Poutinename>
-        {props.attributes.name}
-      </Poutinename>
-      <div className="poutine-score">
-        {props.attributes.avg_score}
-      </div>
+      <Poutinename>{props.attributes.name}</Poutinename>
+      <div className="poutine-score">{props.attributes.avg_score}</div>
       <Linkwrapper>
-        <Link to={`/poutines/${props.attributes.slug}`}> {props.attributes.name} </Link>
+        <Link to={`/poutines/${props.attributes.slug}`}>
+          {" "}
+          {props.attributes.name}{" "}
+        </Link>
       </Linkwrapper>
     </Card>
-  )
-}
+  );
+};
 
-export default Poutine
-
+export default Poutine;
